2eb0ecd1e763e98a121826d229105dc3f848d9fe
zhaosongsong <1739416503@qq.com>
2019-12-25 23:06:04 +0800

    完善404页面

---
+++ src/views/404.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Description: 
+ * @Author: Jensen
+ * @Github: https://github.com/Jensen02
+ * @Date: 2019-09-15 21:59:05
+ * @LastEditors  : Please set LastEditors
+ * @LastEditTime : 2019-12-25 22:52:17
+ -->
+<template>
+	<div class="hello">
+    <h3>Not Found 404</h3>
+	</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+	// @Prop() private msg!: string;
+}
+</script>
+
+<style scoped lang="less">
+</style>
